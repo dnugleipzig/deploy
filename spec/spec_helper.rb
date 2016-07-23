b07e48c44e70a6490437f35c861b44175aa91353
Alexander Groß <agross@therightstuff.de>
2016-07-23 21:02:25 +0200

    Gem update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,9 @@ require 'simplecov/teamcity_service_message_formatter'
 SimpleCov.start(:default)
 
 RSpec.configure do |config|
-  # These two settings work together to allow you to limit a spec run
-  # to individual examples or groups you care about by tagging them with
-  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
-  # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+  config.filter_run_when_matching(:focus)
 
-  config.expect_with :rspec do |c|
+  config.expect_with(:rspec) do |c|
     c.syntax = :expect
   end
 end
